f2f57e682dd1419e05f268009903e3766839c787
joebrockhaus <ninjasecretsauce@outlook.com>
2021-08-27 15:09:05 -0500

    Add Item Generator first pass

--- MatchGame/Program.cs
+++ MatchGame/Program.cs
@@ -15,6 +15,33 @@ namespace MatchGame
 
         public override string ToString() => $"{Id} - {Type} - {Weight}";
     }
+
+    public static class Helper
+    {
+        private static Random _rand = new Random();
+
+        public static List<Item> GetItems(int numCommons, int numRares, int numEpics)
+            => Enumerable
+                .Range(1, numCommons + numRares + numEpics)
+                .Select(id =>
+                    new Item
+                    {
+                        Id = id,
+                        Type = GetItemType(numCommons, numRares, id),
+                        Weight = GetItemType(numCommons, numRares, id) == ItemType.Epic ? 100 : _rand.Next(1, 100)
+                    })
+                .ToList();
+
+        private static ItemType GetItemType(int numCommons, int numRares, int id)
+            =>
+                id <= numCommons
+                    ? ItemType.Common
+                    : id > numCommons && id <= numCommons + numRares
+                        ? ItemType.Rare
+                        : ItemType.Epic;
+
+    }
+
     public class Program
     {
         static void Main(string[] args)
