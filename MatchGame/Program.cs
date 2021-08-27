39b9b5e542c062e4cfa23b826b2ea3a2e3744e20
joebrockhaus <ninjasecretsauce@outlook.com>
2021-08-27 15:03:54 -0500

    Add Item + ItemType Definitions

--- MatchGame/Program.cs
+++ MatchGame/Program.cs
@@ -6,6 +6,15 @@ using System.Linq;
 
 namespace MatchGame
 {
+    public enum ItemType { Common, Rare, Epic }
+    public class Item
+    {
+        public int Id;
+        public int Weight;
+        public ItemType Type;
+
+        public override string ToString() => $"{Id} - {Type} - {Weight}";
+    }
     public class Program
     {
         static void Main(string[] args)
