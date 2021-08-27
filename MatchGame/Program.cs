945ffae3b725356120c3deb288b3561a4086345c
joebrockhaus <ninjasecretsauce@outlook.com>
2021-08-27 15:24:49 -0500

    Re-work Weighted List Generator + Draws based on initial approach
    - Use numeric range versus adding n-items-per-weight to list

--- MatchGame/Program.cs
+++ MatchGame/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 
 namespace MatchGame
@@ -29,6 +27,53 @@ namespace MatchGame
 
     public class Program
     {
+        private static Random rand = new Random();
+
+        private static List<(Item Item, (int L, int U) Range)> GetWeightedItems(IEnumerable<Item> items)
+        {
+            /*
+             * generate a range for each item
+             * given:
+             *  id | weight
+             *  1  | 30
+             *  2  | 45
+             *  ...
+             *  9  | 55
+             *  10 | 35
+             *  
+             * result: 
+             *  (1, (1,30))
+             *  (2, (31,66))
+             *  (3, (67,112))
+             *  (4, (113,168))
+             */
+            var offset = 0;
+            return items
+                .OrderBy(x => x.Weight)
+                .Select(item => (Item: item, Range: (offset + 1, offset += item.Weight)))
+                .ToList();
+        }
+
+        private static IEnumerable<Item> DrawRandomWeightedItemsFromPool(IEnumerable<Item> pool, int cardsToDraw)
+        {
+            var drawn = new List<Item>();
+            var poolCopy = new List<Item>(pool);
+            // get n items
+            for (var i = 0; i < cardsToDraw; i++)
+            {
+                var weightedItems = GetWeightedItems(poolCopy);
+                var totalWeight = poolCopy.Sum(x => x.Weight);
+                // choose a random value between 1 -> totalWeight
+                var rando = rand.Next(1, totalWeight + 1);
+                // select the item with a matching range
+                var chosenItem = weightedItems.First(wi => rando >= wi.Range.L && rando <= wi.Range.U).Item;
+                // remove the item from the pool so we don't select it 2x
+                var rC = poolCopy.RemoveAll(x => x.Id == chosenItem.Id);
+                drawn.Add(chosenItem);
+            }
+            return drawn;
+        }
+
         static void Main(string[] args)
         {
             // generate some items
@@ -36,6 +81,10 @@ namespace MatchGame
             var rares = Helper.GetItems(10, 200, ItemType.Rare);
             var epics = Helper.GetItems(1, 300, ItemType.Epic);
             
+            // draw cards based on the desired distribution 
+            var drawnComms = DrawRandomWeightedItemsFromPool(comms, 5).ToArray();
+            var drawnRares = DrawRandomWeightedItemsFromPool(rares, 2).ToArray();
+            var drawnEpics = DrawRandomWeightedItemsFromPool(epics, 1).ToArray();
 
             Console.ReadLine();
         }
