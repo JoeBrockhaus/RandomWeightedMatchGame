0e6cfc1853c2215e940adcc46f3085912a493f8c
joebrockhaus <ninjasecretsauce@outlook.com>
2021-08-27 15:13:58 -0500

    Add WeightedItemGame class with first-pass draw algorithm

--- MatchGame/Program.cs
+++ MatchGame/Program.cs
@@ -42,11 +42,97 @@ namespace MatchGame
 
     }
 
+    public class GameConfig
+    {
+        public (int Total, int ToSelect) Common { get; set; } = (20, 5);
+        public (int Total, int ToSelect) Rare { get; set; } = (10, 2);
+        public (int Total, int ToSelect) Epic { get; set; } = (1, 1);
+    }
+    public class WeightedItemGame
+    {
+        private readonly Random Rand = new Random();
+        private readonly List<Item> Items;
+        private readonly GameConfig Config;
+
+        public WeightedItemGame() : this(new GameConfig()) { }
+        public WeightedItemGame(GameConfig config)
+        {
+            Config = config;
+            Items = Helper.GetItems(Config.Common.Total, Config.Rare.Total, Config.Epic.Total);
+            AllItems = new List<Item>(Items);
+        }
+
+        public List<Item> AllItems { get; set; }
+
+        private Item DrawWeightedItem()
+        {
+            // sort by weight
+            var sortedItems = Items.OrderBy(x => x.Weight);
+            // total weight of all items 
+            var totalWeight = sortedItems.Sum(x => x.Weight);
+            while (true)
+            {
+                var choice = Rand.Next(totalWeight);
+                foreach (var item in Items)
+                {
+                    // if the choice is less than the current item's weight, select it
+                    if (choice < item.Weight)
+                        return item;
+                    // decrement the choice by the current-item's weight
+                    choice -= item.Weight;
+                }
+            }
+        }
+
+        public IEnumerable<Item> GetWeightedItems()
+        {
+            // keep track of the number of items we've drawn
+            var commons = 0;
+            var rares = 0;
+            var epics = 0;
+
+            while (true)
+            {
+                // should the draw be across all items or within each category of type?
+                var draw = DrawWeightedItem();
+                if (draw.Type == ItemType.Common && commons < Config.Common.ToSelect)
+                {
+                    commons++;
+                    yield return draw;
+                    yield return draw;
+                    Items.Remove(draw);
+                }
+                else if (draw.Type == ItemType.Rare && rares < Config.Rare.ToSelect)
+                {
+                    rares++;
+                    yield return draw;
+                    yield return draw;
+                    Items.Remove(draw);
+                }
+                else if (draw.Type == ItemType.Epic && epics < Config.Epic.ToSelect)
+                {
+                    epics++;
+                    yield return draw;
+                    yield return draw;
+                    Items.Remove(draw);
+                }
+
+                if (commons == (int)ItemType.Common
+                    && rares == (int)ItemType.Rare
+                    && epics == (int)ItemType.Epic)
+                { yield break; }
+            }
+        }
+
+    }
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var game = new WeightedItemGame();
+            var randomItems = game.GetWeightedItems().ToList().OrderBy(x => new Guid()).ToList();
+
         }
     }
 }
